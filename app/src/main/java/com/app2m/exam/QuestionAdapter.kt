349ecae5e5029dbd6b9bfaa11210cd1a8be7dac9
conghao <hao.cong@app2m.com>
2018-08-15 20:05:30 +0800

    添加注释：@JvmOverloads constructor

--- app/src/main/java/com/app2m/exam/QuestionAdapter.kt
+++ app/src/main/java/com/app2m/exam/QuestionAdapter.kt
@@ -7,7 +7,12 @@ import android.widget.LinearLayout
 import android.widget.TextView
 import org.jetbrains.anko.*
 
-class QuestionAdapter(val data: List<String>) : RecyclerView.Adapter<QuestionAdapter.QuestionViewHolder>() {
+/**
+ * 加上@ JvmOverloads 的 目的 是 让 Java 代码 也能 识别 默认 参数
+ * 因为 添加 了 注解 标记， 所以 必须 补上 关键字 constructor
+ * 例：class QuestionAdapter @JvmOverloads constructor(val data: List<String>)
+ */
+class QuestionAdapter (val data: List<String>) : RecyclerView.Adapter<QuestionAdapter.QuestionViewHolder>() {
     val questionUI : QuestionUI by lazy {
         QuestionUI()
     }
